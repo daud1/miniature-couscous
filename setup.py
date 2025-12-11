fe8685df30e2fe79d33ba48ae166e5c78005151d
daud1 <david.mwebaza@outlook.com>
2025-12-11 20:25:31 +0300

    clean up code

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="LLMOPS-3",
+    name="studybuddy",
     version="0.1",
-    author="Sudhanshu",
+    author="daud1",
     packages=find_packages(),
     install_requires=requirements,
 )
--- src/config/settings.py
+++ src/config/settings.py
@@ -1,17 +1,18 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class Settings():
 
+class Settings:
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
     MODEL_NAME = "llama-3.1-8b-instant"
-    
+
     TEMPERATURE = 0.9
 
     MAX_RETRIES = 3
 
 
-settings = Settings()  
+settings = Settings()
